ea09eac42962920defa852cfeb2851bae0d84d8e
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-02-04 13:40:37 +0100

    3.1 MiniQuiz hint optie en strafpunt

--- src/qienMiniQuiz/QienMiniQuizGame.java
+++ src/qienMiniQuiz/QienMiniQuizGame.java
@@ -45,20 +45,37 @@ public class QienMiniQuizGame {
 		
 		int score = 0;
 	
-		System.out.println("******************************** \nwelkom bij de minquiz, have fun! \n******************************** \n\n");
+		System.out.println("**************************************************************************** \n"
+		+ "welkom bij de MINIQUIZ, have fun! \n"
+		+ "Weet je een vraag niet en wil je een hint? typ (hint), \ndit betekent wel dat je een punt aftrek krijgt. \n"
+		+ "Per goed antwoord krijg je 3 punten. \n"
+		+ "**************************************************************************** \n");
 		
 		System.out.println("\nVraag 1: Wat is de hoofdstad van Oostenrijk?");
 		String antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+		if (antwoordvraag1.equals("hint")) {
+			System.out.println("Ook wel bekend als Vienna");
+			System.out.println("Geef nu jouw antwoord");
+			score -= 1;
+			antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+		}
+		
 		if (antwoordvraag1.equals(oplossingVraag1)) {
 			System.out.println("Dat is goed!");
-			score += 1;
+			score += 3;
 			System.out.println("Score is nu: " + score);
 		} else {
 			System.out.println("Dat is fout! \nProbeer het nogmaals");
 			antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+			if (antwoordvraag1.equals("hint")) {
+				System.out.println("Ook wel bekend als Vienna");
+				System.out.println("Geef nu jouw antwoord");
+				score -= 1;
+				antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+			} 
 			if (antwoordvraag1.equals(oplossingVraag1)) {
 				System.out.println("Dat is goed!");
-				score += 1;
+				score += 3;
 			} else {
 				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag1);
 			}
@@ -67,16 +84,30 @@ public class QienMiniQuizGame {
 		
 		System.out.println("\nVraag 2: Hoeveel is 6 x 6 : 2 + 22");
 		String antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+		if (antwoordvraag2.equals("hint")) {
+			System.out.println("36 : 2 + 22");
+			System.out.println("Geef nu jouw antwoord");
+			score -= 1;
+			antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+		}
+		
 		if (antwoordvraag2.equals(oplossingVraag2)) {
 			System.out.println("Dat is goed!");
-			score += 1;
+			score += 3;
 			System.out.println("Score is nu: " + score);
 		} else {
 			System.out.println("Dat is fout! \nProbeer het nogmaals");
 			antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+			if (antwoordvraag2.equals("hint")) {
+				System.out.println("36 : 2 + 22");
+				System.out.println("Geef nu jouw antwoord");
+				score -= 1;
+				antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+			} 
+			
 			if (antwoordvraag2.equals(oplossingVraag2)) {
 				System.out.println("Dat is goed!");
-				score += 1;
+				score += 3;
 			} else {
 				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag2);
 			}
@@ -85,16 +116,30 @@ public class QienMiniQuizGame {
 		
 		System.out.println("\nVraag 3: Welke kleur begint altijd bij het schaken?");
 		String antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+		if (antwoordvraag3.equals("hint")) {
+			System.out.println("... begint en ... wint");
+			System.out.println("Geef nu jouw antwoord");
+			score -= 1;
+			antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+		}
+		
 		if (antwoordvraag3.equals(oplossingVraag3)) {
 			System.out.println("Dat is goed!");
-			score += 1;
+			score += 3;
 			System.out.println("Score is nu: " + score);
 		} else {
 			System.out.println("Dat is fout! \nProbeer het nogmaals");
 			antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+			if (antwoordvraag3.equals("hint")) {
+				System.out.println("... begint en ... wint");
+				System.out.println("Geef nu jouw antwoord");
+				score -= 1;
+				antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+			}
+			
 			if (antwoordvraag3.equals(oplossingVraag3)) {
 				System.out.println("Dat is goed!");
-				score += 1;
+				score += 3;
 			} else {
 				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag3);
 			}
@@ -103,16 +148,29 @@ public class QienMiniQuizGame {
 		
 		System.out.println("\nVraag 4: Wie was de eerste president van Amerika?\n Typ (1) voor Abraham Lincoln\n Typ (2) voor George Washington\n Typ (3) voor Benjamin Franklin");
 		String antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+		if (antwoordvraag4.equals("hint")) {
+			System.out.println("Heeft de Britten verslagen in de Amerikaanse revolutie");
+			System.out.println("Geef nu jouw antwoord");
+			score -= 1;
+			antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+		}
+		
 		if (antwoordvraag4.equals(oplossingVraag4)) {
 			System.out.println("Dat is goed!");
-			score += 1;
+			score += 3;
 			System.out.println("Score is nu: " + score);
 		} else {
 			System.out.println("Dat is fout! \nProbeer het nogmaals");
 			antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+			if (antwoordvraag4.equals("hint")) {
+				System.out.println("Heeft de Britten verslagen in de Amerikaanse revolutie");
+				System.out.println("Geef nu jouw antwoord");
+				score -= 1;
+				antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+			}
 			if (antwoordvraag4.equals(oplossingVraag4)) {
 				System.out.println("Dat is goed!");
-				score += 1;
+				score += 3;
 			} else {
 				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag4);
 			}
