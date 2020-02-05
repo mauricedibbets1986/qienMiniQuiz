6ea37a8bba9f4085159677ee16de07554b0831ce
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-02-05 13:55:42 +0100

    4.5 MiniQuiz random vragen toewijzen aan de spelers

--- src/qienMiniQuiz/QienMiniQuizGame.java
+++ src/qienMiniQuiz/QienMiniQuizGame.java
@@ -41,16 +41,17 @@ public class QienMiniQuizGame {
 	static int aanDeBeurt;
 	static int aantalRondes;
 	static int aantalBeurten;
+	static Random random = new Random();
+	static boolean vraag1Gesteld = false;
+	static boolean vraag2Gesteld = false;
+	static boolean vraag3Gesteld = false;
+	static boolean vraag4Gesteld = false;
+	static boolean vraag5Gesteld = false;
 	
 	public static void main(String[] args){
 		
-				
-		String oplossingVraag1 = "wenen";
-		String oplossingVraag2 = "40";
-		String oplossingVraag3 = "wit";
-		String oplossingVraag4 = "2";
-		String oplossingVraag5 = "ushi";
-		
+
+	
 		int score = 0;
 	
 		System.out.println("**************************************************************************** \n"
@@ -77,7 +78,8 @@ public class QienMiniQuizGame {
 			
 			System.out.println("\n" + "\n" + lijstSpelers.get(aanDeBeurt).getName() + " het is jouw beurt! Beantwoord de volgende vraag: ");
 			// vraag ophalen
-			// aantal beurten en rondes berekenen 5 ronden geweest game is klaar
+			vraagStellen();
+			
 			
 			aantalBeurten++;
 			aantalRondes = aantalBeurten / aantalSpelers;
@@ -87,163 +89,9 @@ public class QienMiniQuizGame {
 			}
 		
 		}
-		System.out.println("\nVraag 1: Wat is de hoofdstad van Oostenrijk?");
-		String antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
-		if (antwoordvraag1.equals("hint")) {
-			System.out.println("Ook wel bekend als Vienna");
-			System.out.println("Geef nu jouw antwoord");
-			score -= 1;
-			antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
-		}
 		
-		if (antwoordvraag1.equals(oplossingVraag1)) {
-			System.out.println("Dat is goed!");
-			score += 3;
-			System.out.println("Score is nu: " + score);
-		} else {
-			System.out.println("Dat is fout! \nProbeer het nogmaals");
-			antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
-			if (antwoordvraag1.equals("hint")) {
-				System.out.println("Ook wel bekend als Vienna");
-				System.out.println("Geef nu jouw antwoord");
-				score -= 1;
-				antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
-			} 
-			if (antwoordvraag1.equals(oplossingVraag1)) {
-				System.out.println("Dat is goed!");
-				score += 3;
-			} else {
-				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag1);
-			}
-			System.out.println("Score is: " + score);
-		}
 		
-		System.out.println("\nVraag 2: Hoeveel is 6 x 6 : 2 + 22");
-		String antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
-		if (antwoordvraag2.equals("hint")) {
-			System.out.println("36 : 2 + 22");
-			System.out.println("Geef nu jouw antwoord");
-			score -= 1;
-			antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
-		}
-		
-		if (antwoordvraag2.equals(oplossingVraag2)) {
-			System.out.println("Dat is goed!");
-			score += 3;
-			System.out.println("Score is nu: " + score);
-		} else {
-			System.out.println("Dat is fout! \nProbeer het nogmaals");
-			antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
-			if (antwoordvraag2.equals("hint")) {
-				System.out.println("36 : 2 + 22");
-				System.out.println("Geef nu jouw antwoord");
-				score -= 1;
-				antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
-			} 
-			
-			if (antwoordvraag2.equals(oplossingVraag2)) {
-				System.out.println("Dat is goed!");
-				score += 3;
-			} else {
-				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag2);
-			}
-			System.out.println("Score is: " + score);
-		}
-		
-		System.out.println("\nVraag 3: Welke kleur begint altijd bij het schaken?");
-		String antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
-		if (antwoordvraag3.equals("hint")) {
-			System.out.println("... begint en ... wint");
-			System.out.println("Geef nu jouw antwoord");
-			score -= 1;
-			antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
-		}
-		
-		if (antwoordvraag3.equals(oplossingVraag3)) {
-			System.out.println("Dat is goed!");
-			score += 3;
-			System.out.println("Score is nu: " + score);
-		} else {
-			System.out.println("Dat is fout! \nProbeer het nogmaals");
-			antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
-			if (antwoordvraag3.equals("hint")) {
-				System.out.println("... begint en ... wint");
-				System.out.println("Geef nu jouw antwoord");
-				score -= 1;
-				antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
-			}
-			
-			if (antwoordvraag3.equals(oplossingVraag3)) {
-				System.out.println("Dat is goed!");
-				score += 3;
-			} else {
-				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag3);
-			}
-			System.out.println("Score is: " + score);
-		}
-		
-		System.out.println("\nVraag 4: Wie was de eerste president van Amerika?\n Typ (1) voor Abraham Lincoln\n Typ (2) voor George Washington\n Typ (3) voor Benjamin Franklin");
-		String antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
-		if (antwoordvraag4.equals("hint")) {
-			System.out.println("Heeft de Britten verslagen in de Amerikaanse revolutie");
-			System.out.println("Geef nu jouw antwoord");
-			score -= 1;
-			antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
-		}
-		
-		if (antwoordvraag4.equals(oplossingVraag4)) {
-			System.out.println("Dat is goed!");
-			score += 3;
-			System.out.println("Score is nu: " + score);
-		} else {
-			System.out.println("Dat is fout! \nProbeer het nogmaals");
-			antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
-			if (antwoordvraag4.equals("hint")) {
-				System.out.println("Heeft de Britten verslagen in de Amerikaanse revolutie");
-				System.out.println("Geef nu jouw antwoord");
-				score -= 1;
-				antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
-			}
-			if (antwoordvraag4.equals(oplossingVraag4)) {
-				System.out.println("Dat is goed!");
-				score += 3;
-			} else {
-				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag4);
-			}
-			System.out.println("Score is: " + score);
-		}
-		
-		System.out.println("\nVraag 5: Welk bekend Japans typetje wordt door Wendy van Dijk gespeeld?");
-		String antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
-		if (antwoordvraag4.equals("hint")) {
-			System.out.println("Hip japans restaurant - s");
-			System.out.println("Geef nu jouw antwoord");
-			score -= 1;
-			antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
-		}
-		
-		if (antwoordvraag5.equals(oplossingVraag5)) {
-			System.out.println("Dat is goed!");
-			score += 3;
-			System.out.println("Score is nu: " + score);
-		} else {
-			System.out.println("Dat is fout! \nProbeer het nogmaals");
-			antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
-			if (antwoordvraag4.equals("hint")) {
-				System.out.println("Hip japans restaurant - s");
-				System.out.println("Geef nu jouw antwoord");
-				score -= 1;
-				antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
-			}
-			if (antwoordvraag5.equals(oplossingVraag5)) {
-				System.out.println("Dat is goed!");
-				score += 3;
-			} else {
-				System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag5);
-			}
-			System.out.println("Score is: " + score);
-		}
-		
+
 		System.out.println("Dat was de quiz jouw score is: " + score);
 		if (score <= 8) {
 			System.out.println("Dat kan beter!");
@@ -258,6 +106,380 @@ public class QienMiniQuizGame {
 			System.out.println("Geweldig! Je bent een Quizmaster");
 		}
 
+	}
+	
+	static void vraagStellen() {
+		
+		String oplossingVraag1 = "wenen";
+		String oplossingVraag2 = "40";
+		String oplossingVraag3 = "wit";
+		String oplossingVraag4 = "2";
+		String oplossingVraag5 = "ushi";
+		
+		boolean vraagGesteld = false;
+		int aantalPunten = 0;
+		while (vraagGesteld == false) {
+			int welkeVraag = 1 +  random.nextInt(5);
+			switch (welkeVraag) {
+			case 1:
+				if (vraag1Gesteld == false) {
+					System.out.println("Wat is de hoofdstad van Oostenrijk?");
+					String antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag1.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Ook wel bekend als Vienna");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag1.equals(oplossingVraag1)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag1.equals("hint")) {
+							System.out.println("Ook wel bekend als Vienna");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag1 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag1.equals(oplossingVraag1)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag1);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
+					vraagGesteld = true;
+					vraag1Gesteld = true;
+				}
+				break;
+				
+			case 2:
+				if (vraag2Gesteld == false) {
+					System.out.println("Hoeveel is 6 x 6 : 2 + 22?");
+					String antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag2.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("36 : 2 + 22");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag2.equals(oplossingVraag2)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag2.equals("hint")) {
+							System.out.println("36 : 2 + 22");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag2 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag2.equals(oplossingVraag2)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag2);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
+					vraagGesteld = true;
+					vraag2Gesteld = true;
+				}
+				break;
+			case 3:
+				if (vraag3Gesteld == false) {
+					System.out.println("Welke kleur begint altijd bij het schaken?");
+					String antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag3.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("... begint en ... wint");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag3.equals(oplossingVraag3)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag3.equals("hint")) {
+							System.out.println("... begint en ... wint");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag3 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag3.equals(oplossingVraag3)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag3);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
+					vraagGesteld = true;
+					vraag3Gesteld = true;
+				}
+				break;
+			case 4:
+				if (vraag4Gesteld == false) {
+					System.out.println("Wie was de eerste president van Amerika?\\n Typ (1) voor Abraham Lincoln\\n Typ (2) voor George Washington\\n Typ (3) voor Benjamin Franklin");
+					String antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag4.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Heeft de Britten verslagen in de Amerikaanse revolutie");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag4.equals(oplossingVraag4)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag4.equals("hint")) {
+							System.out.println("Heeft de Britten verslagen in de Amerikaanse revolutie");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag4 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag4.equals(oplossingVraag4)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag4);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
+					vraagGesteld = true;
+					vraag4Gesteld = true;
+				}
+				break;
+			case 5:
+				if (vraag5Gesteld == false) {
+					System.out.println("Welk bekend Japans typetje wordt door Wendy van Dijk gespeeld?");
+					String antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag5.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Hip japans restaurant - s");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag5.equals(oplossingVraag5)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag5.equals("hint")) {
+							System.out.println("Hip japans restaurant - s");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag5.equals(oplossingVraag4)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag5);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 6:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 6");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 7:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 7");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 8:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 8");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 9:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 9");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 10:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 10");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 11:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 11");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 12:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 12");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 13:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 13");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 14:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 14");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 15:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 15");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 16:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 16");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 17:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 17");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 18:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 18");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 19:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 19");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 20:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 20");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 21:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 21");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 22:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 22");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 23:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 23");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 24:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 24");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 25:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 25");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 26:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 26");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 27:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 27");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 28:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 28");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+			case 29:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 29");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			case 30:
+				if (vraag5Gesteld == false) {
+					System.out.println("vraag 30");
+					vraagGesteld = true;
+					vraag5Gesteld = true;
+				}
+				break;
+			default:
+				vraagGesteld = true;
+				break;
+			}			
+		}		
 	}
 	
 	static void spelersAanmaken() {
@@ -298,5 +520,16 @@ class Speler {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public void verhoogScore(int punten) {
+		this.score += punten;
+	}
+
 	
 }
