e0f70691740bfe589afebef9da3b2e56b50f6dfb
mauricedibbets1986 <mauricedibbets1986@gmail.com>
2020-02-05 14:52:47 +0100

    4.6 miniQuiz 15 random vragen toewijzen

--- src/qienMiniQuiz/QienMiniQuizGame.java
+++ src/qienMiniQuiz/QienMiniQuizGame.java
@@ -47,6 +47,31 @@ public class QienMiniQuizGame {
 	static boolean vraag3Gesteld = false;
 	static boolean vraag4Gesteld = false;
 	static boolean vraag5Gesteld = false;
+	static boolean vraag6Gesteld = false;
+	static boolean vraag7Gesteld = false;
+	static boolean vraag8Gesteld = false;
+	static boolean vraag9Gesteld = false;
+	static boolean vraag10Gesteld = false;
+	static boolean vraag11Gesteld = false;
+	static boolean vraag12Gesteld = false;
+	static boolean vraag13Gesteld = false;
+	static boolean vraag14Gesteld = false;
+	static boolean vraag15Gesteld = false;
+	static boolean vraag16Gesteld = false;
+	static boolean vraag17Gesteld = false;
+	static boolean vraag18Gesteld = false;
+	static boolean vraag19Gesteld = false;
+	static boolean vraag20Gesteld = false;
+	static boolean vraag21Gesteld = false;
+	static boolean vraag22Gesteld = false;
+	static boolean vraag23Gesteld = false;
+	static boolean vraag24Gesteld = false;
+	static boolean vraag25Gesteld = false;
+	static boolean vraag26Gesteld = false;
+	static boolean vraag27Gesteld = false;
+	static boolean vraag28Gesteld = false;
+	static boolean vraag29Gesteld = false;
+	static boolean vraag30Gesteld = false;
 	
 	public static void main(String[] args){
 		
@@ -115,11 +140,22 @@ public class QienMiniQuizGame {
 		String oplossingVraag3 = "wit";
 		String oplossingVraag4 = "2";
 		String oplossingVraag5 = "ushi";
+		String oplossingVraag6 = "4";
+		String oplossingVraag7 = "js";
+		String oplossingVraag71 = "javascript";
+		String oplossingVraag8 = "silicon valley";
+		String oplossingVraag9 = "3";
+		String oplossingVraag12 = "20";
+		String oplossingVraag121 = "20ste";
+		String oplossingVraag13 = "4";
+		String oplossingVraag14 = "scrum";
+
 		
 		boolean vraagGesteld = false;
 		int aantalPunten = 0;
 		while (vraagGesteld == false) {
-			int welkeVraag = 1 +  random.nextInt(5);
+			int welkeVraag = 1 +  random.nextInt(15);
+			
 			switch (welkeVraag) {
 			case 1:
 				if (vraag1Gesteld == false) {
@@ -293,7 +329,7 @@ public class QienMiniQuizGame {
 							aantalPunten -= 1;
 							antwoordvraag5 = mijnScanner.nextLine().toLowerCase();
 						} 
-						if (antwoordvraag5.equals(oplossingVraag4)) {
+						if (antwoordvraag5.equals(oplossingVraag5)) {
 							System.out.println("Dat is goed!");
 							aantalPunten += 3;
 						} else {
@@ -308,171 +344,292 @@ public class QienMiniQuizGame {
 				}
 				break;
 			case 6:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 6");
+				if (vraag6Gesteld == false) {
+					System.out.println("Wat is het snelste dier van de wereld?\n Typ (1) voor Hazewindhond\n Typ (2) voor Gaffelbok\n Typ (3) voor Bulvleermuizen \n Typ (4) voor Jachtluipaard Cheeta");
+					String antwoordvraag6 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag6.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Geel met zwarte stippen");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag6 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag6.equals(oplossingVraag6)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag6 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag6.equals("hint")) {
+							System.out.println("Geel met zwarte stippen");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag6 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag6.equals(oplossingVraag6)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag6);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag6Gesteld = true;
 				}
 				break;
 			case 7:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 7");
+				if (vraag7Gesteld == false) {
+					System.out.println("Van welke taal is REACT een bibliotheek?");
+					String antwoordvraag7 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag7.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Het is geen JAVA");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag7 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag7.equals(oplossingVraag7) || antwoordvraag7.equals(oplossingVraag71)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag7 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag7.equals("hint")) {
+							System.out.println("Het is geen JAVA");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag7 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag7.equals(oplossingVraag7) || antwoordvraag7.equals(oplossingVraag71)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag7);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag7Gesteld = true;
 				}
 				break;
 			case 8:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 8");
+				if (vraag8Gesteld == false) {
+					System.out.println("In Californie is een regio waar veel technologiebedrijven zijn gevestigd. Hoe heet deze regio?");
+					String antwoordvraag8 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag8.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Ook een naam van een serie dat gaat over code geeks die het bedrijg PiedPiper oprichten");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag8 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag8.equals(oplossingVraag8)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag8 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag8.equals("hint")) {
+							System.out.println("Ook een naam van een serie dat gaat over code geeks die het bedrijg PiedPiper oprichten");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag8 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag8.equals(oplossingVraag8)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag8);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag8Gesteld = true;
 				}
 				break;
 			case 9:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 9");
+				if (vraag9Gesteld == false) {
+					System.out.println("Waar staat de afkorting HTML voor? \n Typ (1) voor HyperText Management Language \n Typ (2) voor HotText Marking Language \n Typ (3) voor HyperText Markup Language");
+					String antwoordvraag9 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag9.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("M van Markup");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag9 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag9.equals(oplossingVraag9)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag9 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag9.equals("hint")) {
+							System.out.println("M van Markup");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag9 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag9.equals(oplossingVraag9)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag9);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag9Gesteld = true;
 				}
 				break;
 			case 10:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 10");
+				if (vraag9Gesteld == false) {
+					System.out.println("Geen vraag! 3 Bonuspunten!!");
+					aantalPunten += 3;
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag10Gesteld = true;
 				}
 				break;
 			case 11:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 11");
+				if (vraag11Gesteld == false) {
+					System.out.println("Geen vraag! 1 Strafpunt!!");
+					aantalPunten -= 1;
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag11Gesteld = true;
 				}
 				break;
 			case 12:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 12");
+				if (vraag12Gesteld == false) {
+					System.out.println("Op de hoeveelste van Februari is het Qien Code Face#3?");
+					String antwoordvraag12 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag12.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Op de 3e donderdag van Februari");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag12 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag12.equals(oplossingVraag12) || antwoordvraag12.equals(oplossingVraag121)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag12 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag12.equals("hint")) {
+							System.out.println("Op de 3e donderdag van Februari");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag12 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag12.equals(oplossingVraag12)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag12);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag12Gesteld = true;
 				}
 				break;
 			case 13:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 13");
+				if (vraag13Gesteld == false) {
+					System.out.println("Wie zijn de managing partners van Qien? \n Typ (1) voor Paul & Hamid \n Typ (2) voor Mauk & Hamid \n Typ (3) voor Jack & Paul \n Typ (4) voor Hamid & Dennis");
+					String antwoordvraag13 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag13.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("1 van de managing partners heb je op de 1e dag in de ochtend gezien");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag13 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag13.equals(oplossingVraag13)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag13 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag13.equals("hint")) {
+							System.out.println("1 van de managing partners heb je op de 1e dag in de ochtend gezien");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag13 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag13.equals(oplossingVraag13)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag13);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag13Gesteld = true;
 				}
 				break;
 			case 14:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 14");
+				if (vraag14Gesteld == false) {
+					System.out.println("Gaan wij scrum of agile leren?");
+					String antwoordvraag14 = mijnScanner.nextLine().toLowerCase();
+					if (antwoordvraag14.equals("hint")) {
+						aantalPunten -= 1;
+						System.out.println("Laatste 3 letters geven het favoriete drankje van Jack Sparrow");
+						System.out.println("Geef nu jouw antwoord");
+						antwoordvraag14 = mijnScanner.nextLine().toLowerCase();
+					}
+					
+					if (antwoordvraag14.equals(oplossingVraag14)) {
+						System.out.println("Dat is goed!");
+						aantalPunten += 3;
+					} else {
+						System.out.println("Dat is fout! \nProbeer het nogmaals");
+						antwoordvraag14 = mijnScanner.nextLine().toLowerCase();
+						if (antwoordvraag14.equals("hint")) {
+							System.out.println("Laatste 3 letters geven het favoriete drankje van Jack Sparrow");
+							System.out.println("Geef nu jouw antwoord");
+							aantalPunten -= 1;
+							antwoordvraag14 = mijnScanner.nextLine().toLowerCase();
+						} 
+						if (antwoordvraag14.equals(oplossingVraag14)) {
+							System.out.println("Dat is goed!");
+							aantalPunten += 3;
+						} else {
+							System.out.println("Dat is nog steeds fout het antwoord was: " + oplossingVraag14);
+						}
+						
+					}
+					lijstSpelers.get(aanDeBeurt).verhoogScore(aantalPunten);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag14Gesteld = true;
 				}
 				break;
 			case 15:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 15");
+				if (vraag15Gesteld == false) {
+					System.out.println("Geen vraag! Wat een pech door een error in de code zijn je punten gewist en begin je weer bij 0");
+					aantalPunten = 0;
+					lijstSpelers.get(aanDeBeurt).setScore(0);
+					System.out.println("Score is nu: " + lijstSpelers.get(aanDeBeurt).getScore());
 					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 16:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 16");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 17:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 17");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 18:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 18");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 19:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 19");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 20:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 20");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 21:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 21");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 22:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 22");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 23:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 23");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 24:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 24");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 25:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 25");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 26:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 26");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 27:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 27");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 28:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 28");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-			case 29:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 29");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
-				}
-				break;
-			case 30:
-				if (vraag5Gesteld == false) {
-					System.out.println("vraag 30");
-					vraagGesteld = true;
-					vraag5Gesteld = true;
+					vraag15Gesteld = true;
 				}
 				break;
 			default:
@@ -483,8 +640,8 @@ public class QienMiniQuizGame {
 	}
 	
 	static void spelersAanmaken() {
-		while (aantalSpelers == 0 || aantalSpelers > 4) {
-			System.out.println("Met Hoeveel Spelers wil je spelen? (max 4)");
+		while (aantalSpelers == 0 || aantalSpelers > 3) {
+			System.out.println("Met Hoeveel Spelers wil je spelen? (max 3)");
 			int inputSpelers = mijnScanner.nextInt();
 			if (inputSpelers > 4) {
 				System.out.println("Dat is meer dan het maximum aantal spelers");
